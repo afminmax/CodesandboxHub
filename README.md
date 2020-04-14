38b4a5f69b24350e4a2e8542b404e46b71070803
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2020-04-14 22:33:57 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# CodesandboxHub
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Cobra Mk III</h1>
+    <ul>
+      <li>Fast</li>
+      <li>Light</li>
+      <li>Upgradeable</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
