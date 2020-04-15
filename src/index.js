f11cfe7e9180e942a92324f1f4a3b02804811248
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2020-04-15 17:38:58 +0200

    js expressions in jsx

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const ship = "Viper Mk 4";
+const size = "large";
+
 ReactDOM.render(
   <div>
-    <h1>Cobra Mk III</h1>
+    <h1>Type: {ship}</h1>
+    <h2>Size: {size}</h2>
+    <h2>Item ID: {67 + 9}</h2>
+    <h2>Random plate: {Math.floor(Math.random() * 10) + 5}</h2>
+    <h2>Plate name: {ship + " " + size}</h2>
+    <h2>String Interpolation: {`${ship}`}</h2>
     <ul>
       <li>Fast</li>
       <li>Light</li>
       <li>Upgradeable</li>
+      <li>Iconic</li>
     </ul>
   </div>,
   document.getElementById("root")
