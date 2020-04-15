39955da791ba0224e8106f5556938f2e417c9040
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2020-04-15 17:57:14 +0200

    dynamic js in jsx

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ import ReactDOM from "react-dom";
 
 const ship = "Viper Mk 4";
 const size = "large";
+var date = new Date();
+console.log(date);
+// ReactDOM.render(
+//   <div>
+//     <h1>Type: {ship}</h1>
+//     <h2>Size: {size}</h2>
+//     <h2>Item ID: {67 + 9}</h2>
+//     <h2>Random plate: {Math.floor(Math.random() * 10) + 5}</h2>
+//     <h2>Plate name: {ship + " " + size}</h2>
+//     <h2>String Interpolation: {`${ship}`}</h2>
+//     <ul>
+//       <li>Fast</li>
+//       <li>Light</li>
+//       <li>Upgradeable</li>
+//       <li>Iconic</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
 
 ReactDOM.render(
   <div>
-    <h1>Type: {ship}</h1>
-    <h2>Size: {size}</h2>
-    <h2>Item ID: {67 + 9}</h2>
-    <h2>Random plate: {Math.floor(Math.random() * 10) + 5}</h2>
-    <h2>Plate name: {ship + " " + size}</h2>
-    <h2>String Interpolation: {`${ship}`}</h2>
-    <ul>
-      <li>Fast</li>
-      <li>Light</li>
-      <li>Upgradeable</li>
-      <li>Iconic</li>
-    </ul>
+    <p>Starship Name: {ship}</p>
+    <p>Game year: {date.getFullYear()}</p>
   </div>,
   document.getElementById("root")
 );
