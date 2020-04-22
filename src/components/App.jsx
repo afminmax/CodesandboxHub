6155510e7bd36d272bbda75c8a1b8eee5f554c5d
AF MIN & MAX <50870007+afminmax@users.noreply.github.com>
2020-04-22 17:35:00 +0200

    more practice with jsx
    added import practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 
 import Heading from "./Heading";
 import List from "./List";
+import { add, subtract, multiply, divide } from "./Calculator";
 
 function App() {
   return (
     <div>
       <Heading />
       <List />
+      <h1>And a wee bit of math...</h1>
+      <ul>
+        <li>adding: 15 + 5 = {add(15, 5)}</li>
+        <li>multiplying: 5 * 5 = {multiply(5, 5)}</li>
+        <li>dividing: 25 / 5 = {divide(25, 5)}</li>
+        <li>subtracting: 30 - 5 = {subtract(30, 5)}</li>
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+// import pi, { doublePi, triplePi } from "./math.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// ReactDOM.render(<App />,<ul>
+//   <li>{pi}</li>
+//   <li>{doublePi()}</li>
+//   <li>{triplePi()}</li>
+//   <li>And now for calculator values</li>
+//   </ul>, document.getElementById("root"));
